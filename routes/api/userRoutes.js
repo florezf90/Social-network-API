81d22acb362105ca9ebe00b8b6d30cd385935325
florezf90 <florezf90@gmail.com>
2024-01-01 23:35:44 -0600

    created the routes based on required criteria on the challenge 18, using the controllers created previously for user and thoughts

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,19 @@
 const router = require("express").Router();
-const {} = require("../../controllers/userControllers");
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userControllers");
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
